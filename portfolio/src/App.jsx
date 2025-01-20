e39806c0e6d4ee251df3db8de61a4cf809f6bae3
kpossoli <guilhermelibanio1@gmail.com>
2025-01-20 12:12:43 -0300

    ajuste da pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/Portfolio" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
