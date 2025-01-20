6444defcb6539c1af70e4e045f36ca21c256172d
kpossoli <guilhermelibanio1@gmail.com>
2025-01-20 12:25:14 -0300

    solução

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Tornar Home a página inicial */}
+        <Route path="/" element={<Home />} /> {/* Página inicial com o caminho '/' */}
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
